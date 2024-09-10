667c860852c42f308485c3155258a7d830fd6ef1
murairam <marimiilpalu@gmail.com>
2024-09-10 16:29:17 +0200

    look over quotes

--- srcs/lexing/tokenize.c
+++ srcs/lexing/tokenize.c
@@ -6,7 +6,7 @@
 /*   By: mmiilpal <mmiilpal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 11:31:57 by rbalazs           #+#    #+#             */
-/*   Updated: 2024/09/10 16:14:55 by mmiilpal         ###   ########.fr       */
+/*   Updated: 2024/09/10 16:20:12 by mmiilpal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,25 +64,19 @@ bool	ft_tokenize(char *line, t_token **tokens, t_data *data)
 	is_single_quotes = false;
 	while (line[i] != '\0')
 	{
-		if (line[i] == '"' && !is_single_quotes)
+		if (line[i] == '"')
 		{
 			is_double_quotes = !is_double_quotes;
-			if (is_double_quotes)
-				i++;
-			else
-				ft_append_word_dquotes(tokens, line, &i);
+			i++;
 		}
-		else if (line[i] == '\'' && !is_double_quotes)
+		else if (line[i] == '\'')
 		{
 			is_single_quotes = !is_single_quotes;
-			if (is_single_quotes)
-				i++;
-			else
-				ft_append_word_squotes(tokens, line, &i);
+			i++;
 		}
-		else if (is_double_quotes)
+		else if (is_double_quotes == true)
 			ft_append_word_dquotes(tokens, line, &i);
-		else if (is_single_quotes)
+		else if (is_single_quotes == true)
 			ft_append_word_squotes(tokens, line, &i);
 		else if (ft_isspace(line[i]))
 			i++;
