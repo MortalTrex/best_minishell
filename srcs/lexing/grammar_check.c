1c765217d61c95eddfa65cce0b3c76ff365114e9
murairam <marimiilpalu@gmail.com>
2024-09-12 17:11:59 +0200

    i will have to check the norminette tomorrow

--- srcs/lexing/grammar_check.c
+++ srcs/lexing/grammar_check.c
@@ -6,20 +6,18 @@
 /*   By: mmiilpal <mmiilpal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 12:36:52 by rbalazs           #+#    #+#             */
-/*   Updated: 2024/09/12 15:59:21 by mmiilpal         ###   ########.fr       */
+/*   Updated: 2024/09/12 17:09:56 by mmiilpal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-bool ft_is_operator(char c)
+bool	ft_is_operator(char c)
 {
-	return (c == '|' || c == '(' || c == '>' || c == '<' || c == ')' || c == '&');
-}
-
-bool ft_is_multi_char_operator(const char *str)
-{
-	return (ft_strncmp(str, ">>", 2) == 0 || ft_strncmp(str, "<<", 2) == 0 || ft_strncmp(str, "&&", 2) == 0 || ft_strncmp(str, "||", 2) == 0);
+	if (c == '|' || c == '(' || c == '>' || c == '<' || c == ')' \
+			|| c == '&' )
+		return (true);
+	return (false);
 }
 
 bool	ft_is_quote(char c)
