3103b17bbe044fa8ca01390917a5d889cceaa1d8
Ralph Balazs <rbalazs@made-f0Cr12s4.clusters.42paris.fr>
2024-09-20 17:25:47 +0200

    change export

--- srcs/builtins/export.c
+++ srcs/builtins/export.c
@@ -6,11 +6,25 @@
 /*   By: rbalazs <rbalazs@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 11:32:18 by rbalazs           #+#    #+#             */
-/*   Updated: 2024/09/18 16:55:48 by rbalazs          ###   ########.fr       */
+/*   Updated: 2024/09/20 17:20:16 by rbalazs          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
+
+bool check_double(t_data *data, char *var)
+{
+	t_env *tmp;
+
+	tmp = data->env;
+	while  (tmp)
+	{
+		if (!ft_strcmp(tmp->line, var))
+			return(true);
+		tmp = tmp->next;
+	}
+	return (false);
+}
 
 void	ft_swap_env_lines(t_env *a, t_env *b)
 {
@@ -59,6 +73,46 @@ void	ft_exp_env(t_data *data)
 	}
 }
 
+void change_value (t_data *data, char *old, char *new)
+{
+	t_env *tmp;
+
+	tmp = data->env;
+	while  (tmp)
+	{
+		if (!ft_strcmp(tmp->line, old))
+		{
+			tmp->line = ft_strdup(new);
+			break;
+		}
+		tmp = tmp->next;
+	}
+}
+
+bool	check_change_value(t_data *data)
+{
+	t_token *tmp;
+	char *old_str;
+	char *new_str;
+
+	old_str = NULL;
+	new_str = NULL;
+	tmp = data->tok;
+	while(tmp)
+	{
+		if (!ft_strcmp(tmp->value, "="))
+		{
+			tmp = tmp->next;
+			new_str = ft_strdup(tmp->value);
+			change_value(data, old_str, new_str);
+			return(free(old_str), free(new_str), true);
+		}
+		old_str = ft_strdup(tmp->value);
+		tmp = tmp->next;
+	}
+	return(free(old_str), free(new_str), false);
+}
+
 void	ft_export(t_data *data)
 {
 	t_token	*tmp;
@@ -68,10 +122,13 @@ void	ft_export(t_data *data)
 	tmp = data->tok;
 	after_export = false;
 	i = 0;
-	while (tmp)
+	if (check_change_value(data) == true)
+		i = -1;
+	while (tmp && i >= 0)
 	{
 		if (after_export == true)
-			push_node_to_env(data, tmp->value);
+			if (check_double(data, tmp->value) == false)
+				push_node_to_env(data, tmp->value);
 		if (!ft_strcmp(tmp->value, "export"))
 			after_export = true;
 		tmp = tmp->next;
