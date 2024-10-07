771dc1a62121f3378d17385e2bce829a8437b8d2
murairam <marimiilpalu@gmail.com>
2024-10-07 17:05:57 +0200

    not even sure what i changed

--- srcs/parsing/parser.c
+++ srcs/parsing/parser.c
@@ -6,12 +6,19 @@
 /*   By: mmiilpal <mmiilpal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 15:15:53 by mmiilpal          #+#    #+#             */
-/*   Updated: 2024/10/07 15:55:44 by mmiilpal         ###   ########.fr       */
+/*   Updated: 2024/10/07 16:47:29 by mmiilpal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+bool	is_redir(t_token *token)
+{
+	if (token->type == T_REDIR_IN || token->type == T_REDIR_OUT \
+			||token->type == T_REDIR_HERE || token->type == T_REDIR_APPEND)
+		return (true);
+	return (false);
+}
 void free_ast(t_ast_node *node)
 {
 	if (node == NULL)
@@ -23,7 +30,7 @@ void free_ast(t_ast_node *node)
 
 void parse_tokens(t_data *data)
 {
-	t_ast_node *root = NULL;
+	t_ast_node *root;
 
 	printf("Parsing tokens...\n");
 	if (!check_pipe_syntax(data->tok))
