2a176888235e94a7cf871c0362d44c2420294342
murairam <marimiilpalu@gmail.com>
2024-10-08 14:35:43 +0200

    parsing changes

--- inc/structures.h
+++ inc/structures.h
@@ -6,7 +6,7 @@
 /*   By: mmiilpal <mmiilpal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 11:33:44 by rbalazs           #+#    #+#             */
-/*   Updated: 2024/09/25 17:49:37 by mmiilpal         ###   ########.fr       */
+/*   Updated: 2024/10/08 13:54:32 by mmiilpal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,23 @@
 
 # include <minishell.h>
 
-/* typedef enum e_redir_type
+typedef enum e_redir_type
 {
 	IN,
 	OUT,
 	D_IN,
 	D_OUT
 }					t_redir_type;
+
+typedef enum e_ast_node_type
+{
+	NODE_CMD,
+	NODE_PIPE,
+	NODE_REDIR_IN,
+	NODE_REDIR_OUT,
+	NODE_REDIR_APPEND,
+	NODE_HEREDOC
+}					t_ast_node_type;
 
 typedef struct s_redir
 {
@@ -36,7 +46,16 @@ typedef struct s_cmd
 	// pid_t			pid;
 	struct s_cmd	*next;
 	t_redir			*redir;
-}					t_cmd; */
+}					t_cmd;
+
+typedef struct s_ast_node
+{
+	t_ast_node_type		type;
+	struct s_ast_node	*left;
+	struct s_ast_node	*right;
+	char				*file;
+	t_cmd				*cmd;
+}					t_ast_node;
 
 typedef struct s_env
 {
@@ -48,7 +67,6 @@ typedef struct s_data
 {
 	t_cmd			*cmds;
 	t_env			*env;
-	t_token			*tokens;
 	t_token			*tok;
 	char			*user_line;
 	int				*fd;
--- srcs/parsing/create_node.c
+++ srcs/parsing/create_node.c
@@ -6,10 +6,41 @@
 /*   By: mmiilpal <mmiilpal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 18:03:55 by mmiilpal          #+#    #+#             */
-/*   Updated: 2024/10/07 15:13:33 by mmiilpal         ###   ########.fr       */
+/*   Updated: 2024/10/08 13:38:19 by mmiilpal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+t_ast_node *create_cmd_node(t_cmd *cmd)
+{
+	t_ast_node *node = malloc(sizeof(t_ast_node));
+	node->type = NODE_CMD;
+	node->left = NULL;
+	node->right = NULL;
+	node->cmd = cmd;
+	node->file = NULL;
+	return node;
+}
 
+t_ast_node *create_pipe_node(t_ast_node *left, t_ast_node *right)
+{
+	t_ast_node *node = malloc(sizeof(t_ast_node));
+	node->type = NODE_PIPE;
+	node->left = left;
+	node->right = right;
+	node->cmd = NULL;
+	node->file = NULL;
+	return node;
+}
+
+t_ast_node *create_redir_node(t_ast_node_type type, char *file)
+{
+	t_ast_node *node = malloc(sizeof(t_ast_node));
+	node->type = type;
+	node->left = NULL;
+	node->right = NULL;
+	node->cmd = NULL;
+	node->file = strdup(file); // Make a copy of the file name
+	return node;
+}
