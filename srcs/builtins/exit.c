91058dfda9f9d2f497fee43ff46df7335594445f
mortaltrex <balazsra@gmail.com>
2024-10-21 02:38:29 +0200

    exit function

--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -12,8 +12,52 @@
 
 #include "minishell.h"
 
+bool	ft_is_number(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (false);
+		i++;
+	}
+	return (true);
+}
+
+int	ft_value(int value)
+{
+	if (value > 255)
+		value = value % 256;
+	return (value);
+}
+
 void	ft_exit(t_data *data)
 {
+	t_token	*current;
+	int		value;
+
+	current = data->tok;
+	if (current->next)
+	{
+		current = current->next;
+		if (current->type == T_WORD)
+		{
+			if (ft_is_number(current->value))
+			{
+				value = ft_atoi(current->value);
+				value = ft_value(value);
+				ft_free_all(data);
+				//printf("current->value: %d\n", value);
+				exit(value);
+			}
+			else
+				ft_error(data, "exit: numeric argument required\n");
+		}
+		else
+			ft_error(data, "exit: numeric argument required\n");
+	}
 	ft_free_all(data);
-	exit(EXIT_SUCCESS);
+	exit(0);
 }
