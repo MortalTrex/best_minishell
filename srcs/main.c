8520389c6443576c455d8350bbdf8eb43cb65547
Ralph Balazs <rbalazs@paul-f3Br3s6.clusters.42paris.fr>
2024-10-30 20:50:11 +0100

    Merge with dev

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: rbalazs <rbalazs@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/25 14:17:40 by rbalazs           #+#    #+#             */
-/*   Updated: 2024/10/30 20:36:10 by rbalazs          ###   ########.fr       */
+/*   Updated: 2024/10/30 20:49:40 by rbalazs          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,7 +65,7 @@ int	main(int argc, char **argv, char **envp)
 			continue;
 		//copy_env_char(&data);
 		print_tokens(&data);
-		//parse_tokens(&data);
+		parse_tokens(&data);
 		//ft_execution(&data);
 		data.free_value = 1;
 		ft_free_all(&data);
