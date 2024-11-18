ed34e9ff793dae6afb5480953a2e69fe2ac34a7e
mortaltrex <balazsra@gmail.com>
2024-11-19 00:25:54 +0100

    Infile Outfile work together

--- srcs/exec/exec_redirs.c
+++ srcs/exec/exec_redirs.c
@@ -23,52 +23,48 @@
 
 void	read_outfile(t_ast_node *node, t_data *data)
 {
-	t_ast_node	*current;
+	t_redir *current;
 	int	fd_out;
 
-	current = node;
-	while (current->redir)
+	current = node->redir;
+	while (current)
 	{
-		if (current->redir->type == OUT)
+		if (current->type == OUT)
 		{
-			fd_out = open(current->redir->file, O_WRONLY | O_CREAT | O_TRUNC,
+			fd_out = open(current->file, O_WRONLY | O_CREAT | O_TRUNC,
 					0644);
 			if (fd_out == -1)
 				ft_close_fd(data, "Error opening fd_out");
 			if (dup2(fd_out, STDOUT_FILENO) == -1)
 				ft_close_fd(data, "Error redirecting stdout");
-			if (data->isinfile == true)
-				dup2(data->fd[0], STDIN_FILENO);
 			close(data->fd[0]);
 			close(data->fd[1]);
 			close(fd_out);
 		}
-		current->redir = current->redir->next;
+		current = current->next;
 	}
 }
 
 void	read_infile(t_ast_node *node, t_data *data)
 {
-	t_ast_node	*current;
+	t_redir	*current;
 	int	fd_in;
 
-	current = node;
-	while (current->redir)
+	current = node->redir;
+	while (current)
 	{
-		if (current->redir->type == IN)
+		if (current->type == IN)
 		{
-			fd_in = open(current->redir->file, O_RDONLY);
+			fd_in = open(current->file, O_RDONLY);
 			if (fd_in == -1)
 				ft_close_fd(data, "Error opening fd_in");
 			if (dup2(fd_in, STDIN_FILENO) == -1)
 				ft_close_fd(data, "Error redirecting stdin");
-			if (data->isoutfile == true)
-				dup2(data->fd[1], STDOUT_FILENO);
 			close(data->fd[0]);
 			close(data->fd[1]);
 			close(fd_in);
 		}
-		current->redir = current->redir->next;
+		current = current->next;
 	}
 }
 
