7df4eeacc6bb0cd32c42997e9b0e67dab90428a2
Ralph Balazs <rbalazs@paul-f3Br4s5.clusters.42paris.fr>
2024-11-26 14:54:05 +0100

    cat is working thankyou dollyne

--- srcs/exec/exec_cases.c
+++ srcs/exec/exec_cases.c
@@ -51,14 +51,15 @@ void	multi_pipe(t_ast_node *node, t_data *data, int i)
 		if (node->pid == 0)
 		{
 			dup2(data->fd[1], STDOUT_FILENO);
-			close(data->fd[0]);
-			close(data->fd[1]);
-			close(data->stdin_backup);
-			close(data->stdout_backup);
-			exec(data, node->argv);
+			if (is_builtin(node->argv[0]) == true)
+			{
+				ft_detect_builtin(node->argv, data);
+				ft_free_all(data);
+			}
+			else
+				exec(data, node->argv);
 			exit(1);
 		}
-		waitpid(node->pid, NULL, 1);
 	}
 
 	if (i > 0 && i < data->nb_levels)
@@ -70,12 +71,15 @@ void	multi_pipe(t_ast_node *node, t_data *data, int i)
 		{
 			dup2(data->fd[0], STDIN_FILENO);
 			dup2(data->fd[1], STDOUT_FILENO);
-			close(data->fd[0]);
-			close(data->fd[1]);
-			exec(data, node->argv);
+			if (is_builtin(node->argv[0]) == true)
+			{
+				ft_detect_builtin(node->argv, data);
+				ft_free_all(data);
+			}
+			else
+				exec(data, node->argv);
 			exit(1);
 		}
-		waitpid(node->pid, NULL, 0);
 	}
 
 	if (i == data->nb_levels)
@@ -87,19 +91,22 @@ void	multi_pipe(t_ast_node *node, t_data *data, int i)
 		if (node->pid == 0)
 		{
 			dup2(data->fd[0], STDIN_FILENO);
-			close(data->fd[0]);
-			close(data->fd[1]);
-			exec(data, node->argv);
+			if (is_builtin(node->argv[0]) == true)
+			{
+				ft_detect_builtin(node->argv, data);
+				ft_free_all(data);
+			}
+			else
+				exec(data, node->argv);
 			exit(1);
 		}
 		close(data->fd[0]);
-		close(data->fd[1]);
 		waitpid(node->pid, NULL, 0);
 	}
 
-	if (dup2(data->stdin_backup, STDIN_FILENO) == -1 || dup2(data->stdout_backup,
-            STDOUT_FILENO) == -1)
-        ft_error(data, "Error restoring stdin/stdout");
+	// if (dup2(data->stdin_backup, STDIN_FILENO) == -1 || dup2(data->stdout_backup,
+    //         STDOUT_FILENO) == -1)
+    //     ft_error(data, "Error restoring stdin/stdout");
 }
 
 void	no_pipe(t_ast_node *node, t_data *data)
