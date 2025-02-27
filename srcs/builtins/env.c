b2ffeda3f3e43df8fb8140acb2c16196072061ce
mari <marimiilpalu@gmail.com>
2025-02-27 18:23:17 +0100

    hello

--- srcs/builtins/env.c
+++ srcs/builtins/env.c
@@ -6,17 +6,17 @@
 /*   By: mmiilpal <mmiilpal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 11:32:12 by rbalazs           #+#    #+#             */
-/*   Updated: 2025/02/27 15:05:06 by mmiilpal         ###   ########.fr       */
+/*   Updated: 2025/02/27 17:29:08 by mmiilpal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	ft_env(t_data *shell)
+int	ft_env(t_data *data)
 {
 	t_env	*env;
 
-	env = shell->env;
+	env = data->env;
 	while (env)
 	{
 		printf("%s=%s\n", env->name, env->value);
