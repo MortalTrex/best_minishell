9ed2228e0ddafa7c92f1ec568e5ad9e4e4884ac3
murairam <marimiilpalu@gmail.com>
2025-02-28 16:22:32 +0100

    norminette ok

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: mmiilpal <mmiilpal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/10 13:35:31 by mmiilpal          #+#    #+#             */
-/*   Updated: 2023/11/19 13:40:49 by mmiilpal         ###   ########.fr       */
+/*   Updated: 2025/02/28 16:22:28 by mmiilpal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,8 +36,8 @@ int	main(int argc, char *argv[])
 	}
 	else
 	{
-		printf("input error");
-		return (0);
+		
+		return (printf("input error"), 0);
 	}
 	return (0);
 }*/
