211168fa0439539d1a59821f1d842b9b920636a9
mari <marimiilpalu@gmail.com>
2025-02-28 13:49:22 +0100

    hello

--- srcs/builtins/env_set_free.c
+++ srcs/builtins/env_set_free.c
@@ -6,7 +6,7 @@
 /*   By: mmiilpal <mmiilpal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 15:15:45 by mmiilpal          #+#    #+#             */
-/*   Updated: 2025/02/27 15:21:53 by mmiilpal         ###   ########.fr       */
+/*   Updated: 2025/02/28 13:49:04 by mmiilpal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ void	ft_free_env(t_env *env)
 	while (current)
 	{
 		next = current->next;
+		free(current->line);
 		free(current->name);
 		free(current->value);
 		free(current);
